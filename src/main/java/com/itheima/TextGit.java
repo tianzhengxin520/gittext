0b73b83878ad63994610f6706ef997fff7e919bf
tianzhengxin <1316093754@qq.com>
2018-10-17 01:16:59 +0800

    测试 添加项目

---
+++ src/main/java/com/itheima/TextGit.java
@@ -0,0 +1,11 @@
+package com.itheima;
+
+public class TextGit {
+    public static void main(String[] args) {
+        System.out.println("222222222222");
+        System.out.println("222222222222");
+        System.out.println("222222222222");
+        System.out.println("222222222222");
+        System.out.println("222222222222");
+    }
+}
